caa56147bafed8640c0269281b7b14352a7bded3
tmp2tmp <tmp2tmp@mail2free.com>
2017-08-25 14:02:39 +0900

    "

--- docs/assets/js/default.js
+++ docs/assets/js/default.js
@@ -8,7 +8,7 @@ function searchUp(el, predicate) {
 
 window.onclick=function(ev) {
 //	var pre = searchUp(ev.target, el=>el.classList.contains('highlight'));
-	var pre = searchUp(ev.target, el=>{return el.classList.contains('highlight')});	//for IE11
+	var pre = searchUp(ev.target, function(el){return el.classList.contains('highlight')});	//for M$ IE11
 
 	console.log(pre && pre.localName);
 	if( pre ) {
@@ -31,7 +31,8 @@ window.__onload=function(ev) {
 */
 window.onload=function(ev) {
 //	Array.forEach(document.getElementsByClassName('highlight'),x=>{	//firefox
-	Array.prototype.forEach.call(document.getElementsByClassName('highlight'), x=>{	//M$
+//	Array.prototype.forEach.call(document.getElementsByClassName('highlight'), x=>{	//for M$ Edge
+	Array.prototype.forEach.call(document.getElementsByClassName('highlight'), function(x){	//for M$ IE-11
 		console.log( x.style.height,  getComputedStyle(x).height);
 		x.style.height = getComputedStyle(x).height;
 		x.classList.add('collapse');
