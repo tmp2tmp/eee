bbf7ac1b4c8af53d5538d3474adbe7384587b028
tmp2tmp <tmp2tmp@mail2free.com>
2018-07-23 01:12:09 +0900

    /

--- docs/assets/js/default.js
+++ docs/assets/js/default.js
@@ -55,6 +55,24 @@ window.onload=function(ev) {
 	});
 	console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 	Array.prototype.forEach.call(document.getElementsByClassName('highlight'), function(x){
-		console.log(x.localName);
+		console.log(x.localName, x.style.height,  getComputedStyle(x).height,'---------------------------------');
+		if( x.localName=='pre' ) {
+			x.style.height = getComputedStyle(x).height;
+			x.classList.add('collapse');
+			console.log(x.localName, x.style.hiehgt, x.classList);
+		}
+		else if( x.localName=='div' ) {
+			x.classList.remove('highlight');
+			console.log(x.localName,'-------------------------');
+		}
+		if( x.localName=='pre' ) {
+			x.style.height = getComputedStyle(x).height;
+			x.classList.add('collapse');
+			console.log(x.localName, x.style.hiehgt, x.classList);
+		}
+		else if( x.localName=='div' ) {
+			x.classList.remove('highlight');
+			console.log(x.localName,'-------------------------');
+		}
 	});
 }
